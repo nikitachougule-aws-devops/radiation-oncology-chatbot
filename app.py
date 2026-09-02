80ba680aaa09c6f9c3bf8fbe391aa11f7e5dc03e
Nikita Chougule <126662652+nikitachougule-aws-devops@users.noreply.github.com>
2026-09-02 09:46:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ st.markdown(
         border: 1px solid rgba(255,255,255,0.16);
         border-radius: 14px;
         padding: 0.85rem 0.9rem;
-        margin-top: -0.9rem;
+        margin-top: -0.3rem;
     }
 
     .credit-item {
